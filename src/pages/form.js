156782bae836e7f31282f56107be2f2e752442d1
Nate Costello <natecostello@gmail.com>
2023-12-27 01:13:36 +0000

    adding html form version code back in

--- src/pages/form.js
+++ src/pages/form.js
@@ -1,1 +1,48 @@
+import * as React from "react";
 
+export default function FormPage() {
+  return (
+    <div>
+      <form
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        netlify-honeypot="bot-field"
+        enctype="multipart/form-data"
+      >
+        <input type="hidden" name="bot-field" />
+        <input type="hidden" name="form-name" value="contact" />
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Role:{" "}
+            <select name="role[]" multiple>
+              <option value="leader">Leader</option>
+              <option value="follower">Follower</option>
+            </select>
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message" rows="4"></textarea>
+          </label>
+        </p>
+        <p>
+          <input type="file" name="file-upload" />
+        </p>
+        <p>
+          <input type="submit"></input>
+        </p>
+      </form>
+    </div>
+  );
+}
